67341d8dd229e9bdcfef35317b05c3a5d4d8e191
p.vega <pablovegaroca@gmail.com>
2025-03-20 12:18:07 +0100

    First commit

---
+++ src/main/java/interview/assetsDemo/AssetsDemoApplication.java
@@ -0,0 +1,13 @@
+package interview.assetsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/interview/assetsDemo/AssetsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package interview.assetsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
