5e3d7830c55d04d3c5291a3bd9e28feb5391fe06
p.vega <pablovegaroca@gmail.com>
2025-03-22 15:43:58 +0100

    -First version of r2dbc getByFilter

--- src/main/java/interview/assets/demo/persistence/r2dbc/repositories/IAssetsR2dbcRepository.java
+++ src/main/java/interview/assets/demo/persistence/r2dbc/repositories/IAssetsR2dbcRepository.java
@@ -12,16 +12,13 @@ import reactor.core.publisher.Flux;
 public interface IAssetsR2dbcRepository extends R2dbcRepository<AssetsEntity, Long> {
 
   @Query("SELECT * FROM assets WHERE " +
-      "(:filename IS NULL OR filename LIKE CONCAT('%', :filename, '%')) AND " +
+      "(:filename IS NULL OR filename ILIKE CONCAT('%', :filename, '%')) AND " +
       "(:contentType IS NULL OR content_type = :contentType) AND " +
       "(:startDate IS NULL OR upload_date >= :startDate) AND " +
-      "(:endDate IS NULL OR upload_date <= :endDate) AND " +
-      "ORDER BY upload_date :order")
+      "(:endDate IS NULL OR upload_date <= :endDate) ")
   Flux<AssetsEntity> findByDynamicFilters(
       @Param("filename") String filename,
       @Param("contentType") String contentType,
       @Param("startDate") LocalDateTime startDate,
-      @Param("endDate") LocalDateTime endDate,
-      @Param("order") String order
-  );
+      @Param("endDate") LocalDateTime endDate);
 }
